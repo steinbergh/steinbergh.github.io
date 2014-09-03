687644b7a850d0fd96925aca0896d2d293677799
Henry Steinberg <henryjosephsteinberg@gmail.com>
2014-09-03 17:33:14 -0400

    added favicon

--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,7 @@
   <title>Henry Steinberg</title>
   <link rel="stylesheet" media="screen, projection" href="stylesheets/screen.css">
   <link rel="stylesheet" media="screen, projection" href="fonts/foundation-icons-3/foundation-icons.css">
+	<link rel="icon" type="image/png" href="images/favicon.png">
   <script type="text/javascript" src="scripts/jquery-1.11.1.min.js"></script>
   <script type="text/javascript" src="scripts/velocity.min.js"></script>
   <script type="text/javascript" src="scripts/velocity.ui.min.js"></script>
